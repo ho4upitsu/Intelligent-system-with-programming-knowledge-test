5378ffb8354d32b8af9d5cfeaa256b6c8c0aa9f5
ho4upitsu <ho4upitsu@gmail.com>
2024-11-03 23:44:38 +0200

    add register and login logic

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,26 +1,31 @@
-const pool = require('../db/db');
+const pool = require("../db/db");
+const bcrypt = require("bcrypt");
 
-const createUser = async (req, res) => {
-    const { nickname, email, password } = req.body;
+const register = async (req, res) => {
     try {
+        const { nickname, email, password } = req.body;
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const newUser = await pool.query(
-            'INSERT INTO users (nickname, email, password) VALUES ($1, $2, $3) RETURNING *',
-            [nickname, email, password]
+            "INSERT INTO users (nickname, email, password) VALUES ($1, $2, $3) RETURNING *",
+            [nickname, email, hashedPassword],
         );
         res.status(201).json(newUser.rows[0]);
     } catch (error) {
-        console.error('Error creating user:', error);
-        res.status(500).send('Error creating user');
+        console.error("Error creating user:", error);
+        res.status(500).send("Error creating user");
     }
 };
 
 const getAllUsers = async (req, res) => {
     try {
-        const users = await pool.query('SELECT * FROM users');
+        const users = await pool.query("SELECT * FROM users");
         res.json(users.rows);
     } catch (error) {
-        console.error('Error fetching users:', error);
-        res.status(500).send('Error fetching users');
+        console.error("Error fetching users:", error);
+        res.status(500).send("Error fetching users");
     }
 };
 
@@ -29,25 +34,55 @@ const updateUser = async (req, res) => {
     const { nickname, email } = req.body;
     try {
         const updatedUser = await pool.query(
-            'UPDATE users SET nickname = $1, email = $2 WHERE id = $3 RETURNING *',
-            [nickname, email, id]
+            "UPDATE users SET nickname = $1, email = $2 WHERE id = $3 RETURNING *",
+            [nickname, email, id],
         );
         res.status(200).json(updatedUser.rows[0]);
     } catch (error) {
-        console.error('Error updating user:', error);
-        res.status(500).send('Error updating user');
+        console.error("Error updating user:", error);
+        res.status(500).send("Error updating user");
     }
 };
 
 const deleteUser = async (req, res) => {
     const { id } = req.params;
     try {
-        await pool.query('DELETE FROM users WHERE id = $1', [id]);
+        await pool.query("DELETE FROM users WHERE id = $1", [id]);
         res.status(204).send();
     } catch (error) {
-        console.error('Error deleting user:', error);
-        res.status(500).send('Error deleting user');
+        console.error("Error deleting user:", error);
+        res.status(500).send("Error deleting user");
     }
 };
 
-module.exports = { createUser, getAllUsers, updateUser, deleteUser };
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+            email,
+        ]);
+        if (user.rows.length === 0) {
+            return res.status(404).send("User not found");
+        }
+        const hashedPassword = user.rows[0].password;
+        const isPasswordValid = await bcrypt.compare(password, hashedPassword);
+        if (!isPasswordValid) {
+            return res.status(401).send("Invalid password");
+        }
+        res.status(200).json({
+            message: "Login successfully",
+            user: user.rows[0],
+        });
+    } catch (error) {
+        console.error("Error logging in:", error);
+        res.status(500).send("Error logging in");
+    }
+};
+
+module.exports = {
+    register,
+    getAllUsers,
+    updateUser,
+    deleteUser,
+    login,
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
-const userController = require('../controllers/user');
-const router = require('express').Router();
+const userController = require("../controllers/user");
+const router = require("express").Router();
 
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.post("/register", userController.register);
+router.get("/", userController.getAllUsers);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.post("/login", userController.login);
 
 module.exports = router;
