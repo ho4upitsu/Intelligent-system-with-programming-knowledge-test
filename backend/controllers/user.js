91dcb07ff439bb113db33724dfe7d0e4db82e0d7
ho4upitsu <ho4upitsu@gmail.com>
2024-11-05 23:59:58 +0200

    add using Sequelize

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,28 +1,29 @@
-const pool = require("../db/db");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
     try {
         const { nickname, email, password } = req.body;
         const salt = await bcrypt.genSalt(10);
-
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = await pool.query(
-            "INSERT INTO users (nickname, email, password) VALUES ($1, $2, $3) RETURNING *",
-            [nickname, email, hashedPassword],
-        );
-        res.status(201).json(newUser.rows[0]);
+        const newUser = await User.create({
+            nickname,
+            email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json(newUser);
     } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).send("Error creating user");
+        console.error("Error creating user", error);
+        res.status(500).send(error);
     }
 };
 
 const getAllUsers = async (req, res) => {
     try {
-        const users = await pool.query("SELECT * FROM users");
-        res.json(users.rows);
+        const users = await User.findAll();
+        res.status(200).json(users);
     } catch (error) {
         console.error("Error fetching users:", error);
         res.status(500).send("Error fetching users");
@@ -33,11 +34,14 @@ const updateUser = async (req, res) => {
     const { id } = req.params;
     const { nickname, email } = req.body;
     try {
-        const updatedUser = await pool.query(
-            "UPDATE users SET nickname = $1, email = $2 WHERE id = $3 RETURNING *",
-            [nickname, email, id],
-        );
-        res.status(200).json(updatedUser.rows[0]);
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        user.nickname = nickname;
+        user.email = email;
+        await user.save();
+        res.status(200).json(user);
     } catch (error) {
         console.error("Error updating user:", error);
         res.status(500).send("Error updating user");
@@ -47,8 +51,12 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
     const { id } = req.params;
     try {
-        await pool.query("DELETE FROM users WHERE id = $1", [id]);
-        res.status(204).send();
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        await user.destroy();
+        res.status(204).send("User deleted successfully");
     } catch (error) {
         console.error("Error deleting user:", error);
         res.status(500).send("Error deleting user");
@@ -58,20 +66,17 @@ const deleteUser = async (req, res) => {
 const login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-            email,
-        ]);
-        if (user.rows.length === 0) {
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
             return res.status(404).send("User not found");
         }
-        const hashedPassword = user.rows[0].password;
-        const isPasswordValid = await bcrypt.compare(password, hashedPassword);
-        if (!isPasswordValid) {
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
             return res.status(401).send("Invalid password");
         }
         res.status(200).json({
             message: "Login successfully",
-            user: user.rows[0],
+            user,
         });
     } catch (error) {
         console.error("Error logging in:", error);
--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,13 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
 
-const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST || 'localhost',
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT || 5432,
-});
+const sequelize = new Sequelize(
+    process.env.PGDATABASE,
+    process.env.PGUSER,
+    process.env.PGPASSWORD,
+    {
+        host: process.env.PGHOST,
+        dialect: "postgres",
+    },
+);
 
-module.exports = pool;
+module.exports = sequelize;
